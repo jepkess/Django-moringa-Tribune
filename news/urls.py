5ec1f1581b7fdc425c1564fbcf6fb37cf2130aa9
jepkess <biivincent1997@gmail.com>
2021-11-22 17:51:23 +0300

    add a convert_dates view function that get the weekday number by the date

--- news/urls.py
+++ news/urls.py
@@ -5,5 +5,8 @@ from . import views
 #list of urls instance for our application.
 urlpatterns=[
     url('^$',views.welcome, name='welcome'),
+    url('^today/$',views.news_of_day, name='newsToday')
 ]
+
+#url pattern that match the url today and 
 
--- news/views.py
+++ news/views.py
@@ -12,13 +12,26 @@ def welcome(request):  # welcome is our view function.
 #view function that is responsible for returning news of the specific day.
 def news_of_day(request): 
     date = dt.date.today() # calling the date.today function to get the current date.
+
+    #function that convert the date object to find specific day.
+    day= convert_dates(date)
     html = f'''
         <html>
             <body>
-               <h1>{date.day}-{date.month}-{date.year}</h1>
+               <h1> News for {day} {date.day}-{date.month}-{date.year}</h1>
             </body>
         </html>
     </html>    
     
     '''
     return HttpResponse(html)
+#function that get the weekday number by the date.
+def convert_dates(dates):
+    day_number=dt.date.weekday(dates) # function that takes in date and return a number that represent specific day of the week.
+
+    days=['monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+    #Returning the actual day of the week.
+    day=days[day_number]
+    return day
+
+
