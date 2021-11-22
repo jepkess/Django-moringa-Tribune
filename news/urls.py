bb21531904db45ca3f6a09fb4c9a7a58ec266816
jepkess <biivincent1997@gmail.com>
2021-11-23 00:30:33 +0300

    create base.html file and enxtends in the past-news.html and today-news.html functions

--- news/urls.py
+++ news/urls.py
@@ -1,14 +1,13 @@
 from django.conf.urls import url
-from django.urls import path
 from . import views 
+from django.urls import path
 
 
 #list of urls instance for our application.
 urlpatterns=[
-    # url('^$',views.welcome, name='welcome'),
     path('',views.welcome, name='welcome'),
-    path('today',views.news_of_day, name='newsToday'), #urlpattern for the new_of_ day view function.
-    url(r'archives/(\d{4}-\d{2}-\d{2})',views.past_days_news, name='pastNews')
+    url('^today/$',views.news_of_day, name='newsToday'), #urlpattern for the new_of_ day view function.
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news, name='pastNews')
 ]
 
 #url pattern that match the url today and 
