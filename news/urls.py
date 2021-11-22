af95c37a7705d324a96f1ba5d6809e288e100726
jepkess <biivincent1997@gmail.com>
2021-11-22 16:07:03 +0300

    creating a view function that return the news of the specific date

--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from . import views 
 
+
 #list of urls instance for our application.
 urlpatterns=[
     url('^$',views.welcome, name='welcome'),
--- news/views.py
+++ news/views.py
@@ -1,5 +1,24 @@
 from django.http import HttpResponse
+import datetime as dt
 
-#creating views here 
-def welcome(request): # welcome is our view function.
+# creating views here
+
+
+def welcome(request):  # welcome is our view function.
     return HttpResponse('Welcome to the moringa Tribune')
+
+# adding the dynamic content to our application.
+
+#view function that is responsible for returning news of the specific day.
+def news_of_day(request): 
+    date = dt.date.today() # calling the date.today function to get the current date.
+    html = f'''
+        <html>
+            <body>
+               <h1>{date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+    </html>    
+    
+    '''
+    return HttpResponse(html)
