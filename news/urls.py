d45c86b587bd0973ab4fbf372cf664f4e5827e82
jepkess <biivincent1997@gmail.com>
2021-11-22 14:24:31 +0300

    connecting urlconf of the news app to the project

--- news/urls.py
+++ news/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views 
 
 #list of urls instance for our application.
-urlPatterns=[
-    url('^&',views.welcome, name='welcome'),
+urlpatterns=[
+    url('^$',views.welcome, name='welcome'),
 ]
+
