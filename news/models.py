cca42fd8e02832cd55f97833cf1eaedee8e78f38
jepkess <biivincent1997@gmail.com>
2021-11-23 22:30:51 +0300

    creating models editor,tags and and article and migrate to the database

--- news/models.py
+++ news/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+
+# creating the editor models
+class Editor(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name
+    def save_editor(self):
+        self.save()
+#creating the tags model
+class tags(models.Model):
+    name = models.CharField(max_length =30)
+    def __str__(self):
+        return self.name
+
+#creating the Article model
+class Article(models.Model):
+    title = models.CharField(max_length =60)
+    post = models.TextField()
+    editor = models.ForeignKey(Editor,on_delete=models.CASCADE)
+    tags = models.ManyToManyField(tags)
+    pub_date = models.DateTimeField(default=datetime.datetime.now)
+    
+
+
+    
+   
+   
