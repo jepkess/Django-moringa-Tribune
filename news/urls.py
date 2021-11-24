9b3508da81e59505258a20665da280356b3b6c87
jepkess <biivincent1997@gmail.com>
2021-11-24 23:18:45 +0300

    generate the url path for the news_today function

--- news/urls.py
+++ news/urls.py
@@ -6,7 +6,7 @@ from django.urls import path
 #list of urls instance for our application.
 urlpatterns=[
    
-    path('',views.news_of_day, name='newsToday'), #urlpattern for the new_of_ day view function.
+    path('',views.news_today, name='newsToday'), #urlpattern for the new_of_ day view function.
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news, name='pastNews')
 ]
 
