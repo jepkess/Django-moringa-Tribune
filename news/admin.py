493e997ff2b1f464057395e5eec46d9efe9f5423
jepkess <biivincent1997@gmail.com>
2021-11-24 10:43:45 +0300

    creating the articleadmin class and pass in admin.modeladmin

--- news/admin.py
+++ news/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from .models import Editor,tags,Article
 
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    filter_horizontal=('tags',)
+#filter_Horizontal allows ordering of many to many fields.    
+
+
 admin.site.register(Editor)
-admin.site.register(Article)
+admin.site.register(Article,ArticleAdmin)
 admin.site.register(tags)
