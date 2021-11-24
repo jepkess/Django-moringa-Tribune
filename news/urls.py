396db592546d81fcba49e0deeadb46f2354a1ad6
jepkess <biivincent1997@gmail.com>
2021-11-24 10:45:14 +0300

    deleting the url path for the views.welcome

--- news/urls.py
+++ news/urls.py
@@ -5,8 +5,8 @@ from django.urls import path
 
 #list of urls instance for our application.
 urlpatterns=[
-    path('',views.welcome, name='welcome'),
-    url('^today/$',views.news_of_day, name='newsToday'), #urlpattern for the new_of_ day view function.
+   
+    path('',views.news_of_day, name='newsToday'), #urlpattern for the new_of_ day view function.
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news, name='pastNews')
 ]
 
