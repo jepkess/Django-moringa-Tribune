bc0e3595c0f0b907b1feb5281d5f573537382362
jepkess <biivincent1997@gmail.com>
2021-11-25 12:42:18 +0300

    creating tha urlpattern for the search_results view function

--- news/urls.py
+++ news/urls.py
@@ -7,7 +7,8 @@ from django.urls import path
 urlpatterns=[
    
     path('',views.news_today, name='newsToday'), #urlpattern for the new_of_ day view function.
-    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news, name='pastNews')
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news, name='pastNews'),
+    path('search',views.search_results, name='search_results')
 ]
 
 #url pattern that match the url today and 
